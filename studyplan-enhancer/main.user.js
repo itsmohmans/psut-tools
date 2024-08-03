10ea9dfd50d8f3676f2be1efaf113ab4cb7ff86f
Mohamed Mansour <mohmans@pm.me>
2024-08-03 14:27:51 +0300

    feat(studyplan-enhancer): save selected courses to localStorage

--- studyplan-enhancer/main.user.js
+++ studyplan-enhancer/main.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://portal.psut.edu.jo:5050/StudentServices/StudentStudyPlan.aspx
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @author      Mo Mansour
 // @description 7/24/2023, 10:14:37 PM:
 //  A simple script to add some styling and functionalities to PSUT's study plan page.
@@ -11,6 +11,7 @@
 //  - Different colors for passed, registered, and remaining courses
 //  - Checkboxes to select courses, highlighted with a different color
 //  - A flaoting box that keeps track of credits hours of the selected courses
+//  - Saves selected courses to localStorage
 // ==/UserScript==
 
 if (location.pathname === '/StudentServices/StudentStudyPlan.aspx') { // only on the study plan page
@@ -36,6 +37,17 @@ if (location.pathname === '/StudentServices/StudentStudyPlan.aspx') { // only on
     registered: 0,
     selected: 0
   };
+  let selectedCourses = [];
+  const STORAGE_KEY = "selected_courses";
+
+  let storedCourses;
+  try {
+    storedCourses = JSON.parse(localStorage.getItem(STORAGE_KEY))
+  } catch(e) {
+    console.error("Error while retrieving stored courses:\n", e)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify([]))
+    storedCourses = []
+  }
 
   // get rows of the study plan
   const allTrs = document.getElementsByTagName('tr') // select all <tr> elements in the document because the layout and semantics of the page is beyond dogshit
@@ -48,7 +60,9 @@ if (location.pathname === '/StudentServices/StudentStudyPlan.aspx') { // only on
 
   // get rows of the passed courses
   const passed = Object.keys(rows).filter(
-    row => rows[row].innerText.includes(strings.pass.ar) || rows[row].innerText.includes(strings.pass.en)).map(el => rows[el])
+    row => rows[row].innerText.includes(strings.pass.ar)
+          || rows[row].innerText.includes(strings.pass.en)
+  ).map(el => rows[el])
 
   // Credit hours counter
   let chStyle = {
@@ -85,25 +99,36 @@ if (location.pathname === '/StudentServices/StudentStudyPlan.aspx') { // only on
   Object.keys(tables)
     .map(t => tables[t].getElementsByTagName('tr')[0])
     .forEach(
-      tr => tr.insertAdjacentHTML('beforeend', '<th>Check</th>')
+      tr => tr.insertAdjacentHTML('beforeend', '<th>Select</th>')
     )
   rows.forEach(tr => {
     tr.insertAdjacentHTML(
       'beforeend',
-      '<td> <input type=checkbox style="width: 100%; scale: 1.5; cursor: pointer"/> </td>'
+      '<td> <input class="course-select" type=checkbox style="width: 100%; scale: 1.5; cursor: pointer"/> </td>'
     )
+
     // listen for checkboxes changes
-    tr.querySelector('input').addEventListener('change', (e) => {
+    const input = tr.querySelector('input.course-select')
+    input.addEventListener('change', (e) => {
       if (e.target.checked) {
         credits.selected += Number(tr.cells[2].innerText) // credit hours of selected course
         tr.setAttribute('style', `background-color: ${colors.selected};`)
+        selectedCourses.push(tr.cells[0].innerText)
       }
       else {
         credits.selected -= Number(tr.cells[2].innerText)
         tr.setAttribute('style', `background-color: ${colors.base};`)
+        const idx = selectedCourses.indexOf(tr.cells[0].innerText)
+        if (idx !== -1) { // remove course
+          selectedCourses.splice(idx, 1)
+        }
       }
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(selectedCourses))
       document.getElementById('selected-chs-count').innerText = credits.selected
     })
+    if (storedCourses && storedCourses.includes(tr.cells[0].innerText)) {
+      input.click() // triggers the event listener above
+    }
   })
 
   registered.forEach(tr => {
